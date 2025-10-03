6e944aee51c40341b918409a6be2f5fff39c5743
carlklagba <klagba@gmail.com>
2025-10-03 22:52:28 +0200

    test to remove phase click

--- main/main.go
+++ main/main.go
@@ -15,6 +15,7 @@ import (
 
 const sampleRate = 44100
 const amplitude = 0.5
+const noteOff = 0
 
 var (
 	notesMu        sync.Mutex
@@ -56,16 +57,40 @@ func main() {
 	for i := 0; i <= 127; i++ {
 		noteToFreq[uint32(i)] = midiNoteToFreq(uint8(i))
 	}
+	noteToFreq[noteOff] = 0.0 // Fréquence 0 pour la note "off"
 
 	phase := 0.0
-	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, 0, func(out []float32) {
+	phaseStep := 1.0 / sampleRate
+	var lastNote uint32
+	var fadeInSample uint16 = 441 // 10ms de fade-in
+	var fadeInCount uint16 = 0
+	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, 256, func(out []float32) {
+		note := lastPlayedNote.Load()
+		if note != lastNote {
+			lastNote = note
+			fadeInCount = 0
+		}
+		freq := noteToFreq[note]
+		amp := amplitude
 		for i := range out {
-			freq := noteToFreq[lastPlayedNote.Load()]
-			out[i] = float32(amplitude * math.Sin(2*math.Pi*freq*phase))
-			phase += 1.0 / sampleRate
-			if phase >= 1.0 {
-				phase -= 1.0
+			if fadeInCount < fadeInSample {
+				amp = amplitude * float64(fadeInCount) / float64(fadeInSample)
+				fadeInCount++
 			}
+			out[i] = float32(amp * math.Sin(2*math.Pi*freq*phase))
+
+			phase = phase + phaseStep
+
+			if phase >= 1.0 {
+				phaseStep = -1 * phaseStep
+				phase = phase + phaseStep
+			} else if phase <= 0.0 {
+				phaseStep = -1 * phaseStep
+				phase = phase + phaseStep
+			}
+
+			//_, phase = math.Modf(phase + freq/sampleRate) // fréquence par défaut 440 Hz
+
 		}
 	})
 	must(err)
@@ -87,7 +112,7 @@ func main() {
 				lastPlayedNote.Store(uint32(note))
 			} else if (canal == 0x80) || (canal == 0x90 && velocity == 0) { // Note OFF
 				if lastPlayedNote.Load() == uint32(note) {
-					lastPlayedNote.Store(0)
+					lastPlayedNote.Store(noteOff)
 				}
 			}
 		}),
@@ -100,6 +125,48 @@ func main() {
 	time.Sleep(1000 * time.Second)
 
 }
+
+/*
+func main() {
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+	s := newStereoSine(256, 320, sampleRate)
+	defer s.Close()
+	chk(s.Start())
+	time.Sleep(10 * time.Second)
+	chk(s.Stop())
+}
+
+type stereoSine struct {
+	*portaudio.Stream
+	stepL, phaseL float64
+	stepR, phaseR float64
+}
+
+func newStereoSine(freqL, freqR, sampleRate float64) *stereoSine {
+	s := &stereoSine{nil, freqL / sampleRate, 0, freqR / sampleRate, 0}
+	var err error
+	s.Stream, err = portaudio.OpenDefaultStream(0, 2, sampleRate, 0, s.processAudio)
+	chk(err)
+	return s
+}
+
+func (g *stereoSine) processAudio(out [][]float32) {
+	for i := range out[0] {
+		out[0][i] = float32(math.Sin(2 * math.Pi * g.phaseL))
+		_, g.phaseL = math.Modf(g.phaseL + g.stepL)
+		out[1][i] = float32(math.Sin(2 * math.Pi * g.phaseR))
+		_, g.phaseR = math.Modf(g.phaseR + g.stepR)
+	}
+}
+
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+*/
 
 /*
 func main() {
