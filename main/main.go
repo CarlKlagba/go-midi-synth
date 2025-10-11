9788ec18c102819e3b08f896f6572c03263dfd49
carlklagba <klagba@gmail.com>
2025-10-11 18:43:23 +0200

    refacto

--- main/main.go
+++ main/main.go
@@ -24,60 +24,21 @@ type NotesPlayed struct {
 }
 
 var (
-	atomicPlayedNotes atomic.Value
-	playedNotes       []uint8
-	noteToFreq        = make(map[uint32]float64)
-	noteToPhase       = make(map[uint32]float64)
-	noteToFadeIn      = make(map[uint32]uint16)
+	playedNotes []uint8
 )
 
 func main() {
 	must(portaudio.Initialize())
 	defer portaudio.Terminate()
 
-	atomicPlayedNotes.Store(NotesPlayed{notes: make([]uint8, 0, 10)})
-
-	// Initialiser les fréquences des notes MIDI
-	for i := 0; i <= 127; i++ {
-		noteToFreq[uint32(i)] = midiNoteToFreq(uint8(i))
-	}
-	noteToFreq[noteOff] = 0.0
-
-	// Initialiser les phases des notes MIDI
-	for i := 0; i <= 127; i++ {
-		noteToPhase[uint32(i)] = 0.0
-	}
-
-	// Initialiser les fades-in des notes MIDI
-	for i := 0; i <= 127; i++ {
-		noteToFadeIn[uint32(i)] = 0
-	}
-
-	var fadeInSample uint16 = 441 // 10ms de fade-in
-	var fadeInCount uint16 = 0
-	amp := amplitude
-	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, 256, func(out []float32) {
-		notesPlayed := atomicPlayedNotes.Load().(NotesPlayed)
-		for i := range out {
-			if fadeInCount < fadeInSample {
-				amp = amplitude * float64(fadeInCount) / float64(fadeInSample)
-				fadeInCount++
-			}
-
-			o := float32(0.0)
-			for _, v := range notesPlayed.notes {
-				freq := noteToFreq[uint32(v)]
-				phase := noteToPhase[uint32(v)]
-				step := freq / sampleRate
-
-				o += float32(amp * math.Sin(2*math.Pi*phase))
-
-				_, noteToPhase[uint32(v)] = math.Modf(phase + step)
-			}
-
-			out[i] = o
-		}
-	})
+	wp := newWaveProcessor()
+	stream, err := portaudio.OpenDefaultStream(
+		0,
+		1,
+		sampleRate,
+		256,
+		wp.processAudio,
+	)
 	must(err)
 	defer stream.Close()
 	must(stream.Start())
@@ -107,31 +68,7 @@ func main() {
 
 	rd := reader.New(
 		reader.NoLogger(),
-		reader.Each(func(pos *reader.Position, msg midi.Message) {
-			midiBytes := msg.Raw()
-			if len(midiBytes) < 3 {
-				return
-			}
-			canal := midiBytes[0] & 0xF0
-			note := midiBytes[1]
-			velocity := midiBytes[2]
-			fmt.Printf("Canal: 0x%X, Note: %d, Velocity: %d\n", canal, note, velocity)
-
-			if canal == midiNoteOn && velocity > 0 { // Note ON
-				playedNotes = append(playedNotes, note)
-				n := NotesPlayed{notes: playedNotes}
-				atomicPlayedNotes.Store(n)
-			} else if (canal == midiNoteOff) || (canal == midiNoteOn && velocity == 0) { // Note OFF
-				for i, n := range playedNotes {
-					if n == note {
-						playedNotes = append(playedNotes[:i], playedNotes[i+1:]...)
-						break
-					}
-				}
-				n := NotesPlayed{notes: playedNotes}
-				atomicPlayedNotes.Store(n)
-			}
-		}),
+		reader.Each(listenMidiMessage(wp.atomicPlayedNotes)),
 	)
 
 	fmt.Println("En attente de messages MIDI...")
@@ -142,8 +79,110 @@ func main() {
 
 }
 
+type WaveProcessor struct {
+	atomicPlayedNotes atomic.Value
+	noteToFreq        map[uint32]float64
+	noteToPhase       map[uint32]float64
+	noteToFadeIn      map[uint32]uint16
+	fadeInCount       uint16
+	fadeInSample      uint16
+	amp               float64
+}
+
+func newWaveProcessor() *WaveProcessor {
+	noteToFreq := make(map[uint32]float64)
+	noteToPhase := make(map[uint32]float64)
+	noteToFadeIn := make(map[uint32]uint16)
+
+	var atomicPlayedNotes atomic.Value
+	atomicPlayedNotes.Store(NotesPlayed{notes: make([]uint8, 0, 10)})
+
+	// Initialiser les fréquences des notes MIDI
+	for i := 0; i <= 127; i++ {
+		noteToFreq[uint32(i)] = midiNoteToFreq(uint8(i))
+	}
+	noteToFreq[noteOff] = 0.0
+
+	// Initialiser les phases des notes MIDI
+	for i := 0; i <= 127; i++ {
+		noteToPhase[uint32(i)] = 0.0
+	}
+
+	// Initialiser les fades-in des notes MIDI
+	for i := 0; i <= 127; i++ {
+		noteToFadeIn[uint32(i)] = 0
+	}
+	return &WaveProcessor{
+		atomicPlayedNotes,
+		noteToFreq,
+		noteToPhase,
+		noteToFadeIn,
+		441, // 10ms de fade-in
+		0,
+		amplitude,
+	}
+}
 func midiNoteToFreq(note uint8) float64 {
 	return 440.0 * math.Pow(2, (float64(note)-69)/12)
+}
+
+func (w *WaveProcessor) processAudio(out []float32) {
+	notesPlayed := w.atomicPlayedNotes.Load().(NotesPlayed)
+	for i := range out {
+		if w.fadeInCount < w.fadeInSample {
+			w.amp = amplitude * float64(w.fadeInCount) / float64(w.fadeInSample)
+			w.fadeInCount++
+		}
+
+		o := float32(0.0)
+		for _, v := range notesPlayed.notes {
+			freq := w.noteToFreq[uint32(v)]
+			phase := w.noteToPhase[uint32(v)]
+			step := freq / sampleRate
+
+			o += float32(w.amp * sinWave(phase))
+
+			_, w.noteToPhase[uint32(v)] = math.Modf(phase + step)
+		}
+
+		out[i] = o
+	}
+}
+
+func sinWave(phase float64) float64 {
+	return math.Sin(2 * math.Pi * phase)
+}
+
+func sqrWave(phase float64) float64 {
+	return math.Copysign(1, math.Sin(2*math.Pi*phase))
+}
+
+func listenMidiMessage(atomicPlayedNotes atomic.Value) func(pos *reader.Position, msg midi.Message) {
+	return func(pos *reader.Position, msg midi.Message) {
+		midiBytes := msg.Raw()
+		if len(midiBytes) < 3 {
+			return
+		}
+		canal := midiBytes[0] & 0xF0
+		note := midiBytes[1]
+		velocity := midiBytes[2]
+		fmt.Printf("Canal: 0x%X, Note: %d, Velocity: %d\n", canal, note, velocity)
+
+		if canal == midiNoteOn && velocity > 0 { // Note ON
+			playedNotes = append(playedNotes, note)
+			n := NotesPlayed{notes: playedNotes}
+			atomicPlayedNotes.Store(n)
+		} else if (canal == midiNoteOff) || (canal == midiNoteOn && velocity == 0) { // Note OFF
+			for i, n := range playedNotes {
+				if n == note {
+					playedNotes = append(playedNotes[:i], playedNotes[i+1:]...)
+					break
+				}
+			}
+			n := NotesPlayed{notes: playedNotes}
+			atomicPlayedNotes.Store(n)
+		}
+	}
 }
 
 func must(err error) {
